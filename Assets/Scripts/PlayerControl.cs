1014c733c3f723fc3876930200490ad37d12c325
AtillaGunes <atilla.gunesss@gmail.com>
2021-04-20 02:02:55 +0300

    Menu + gameplay tutorial level

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -16,23 +16,23 @@ public class PlayerControl : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.A)){
                     transform.position += Vector3.left * Time.deltaTime * 130.0f;
-                    transform.Rotate( 0, 0, 0.2f);
+                    transform.Rotate( 0, 0, 0.4f);
         }
         if(Input.GetKey(KeyCode.D)){
                     transform.position += Vector3.right * Time.deltaTime * 130.0f;
-                    transform.Rotate( 0, 0, -0.2f);
+                    transform.Rotate( 0, 0, -0.4f);
 
 
         }
         if(Input.GetKey(KeyCode.W)){
                    transform.position += Vector3.up * Time.deltaTime * 130.0f;
-                   transform.Rotate( -0.1f, 0, 0);
+                   transform.Rotate( -0.2f, 0, 0);
              
 
         }
         if(Input.GetKey(KeyCode.S)){
                     transform.position += Vector3.down * Time.deltaTime * 130.0f;
-                     transform.Rotate( 0.1f, 0, 0);
+                     transform.Rotate( 0.2f, 0, 0);
 
 
         }
